245f0474cf6de0499593af7d220ee10077257ab2
Konstantin <konstantinkarasenko@gmail.com>
2025-04-14 17:00:24 +0300

    Удалены лишние файлы, переименованы существующие, requirements.txt изменён

--- forms/codeform.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class CodeForm(FlaskForm):
-    code = StringField('Code', validators=[DataRequired()])
-    recaptcha = RecaptchaField()
-    submit = SubmitField('Войти')
